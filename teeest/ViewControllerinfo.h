7ba1545a3e0b954838926fcb2512f7edea9c8e00
Janus Broch Mols <janusbrochmols@Januss-MacBook-Pro.local>
2014-11-09 23:13:03 +0100

    .

---
+++ teeest/ViewControllerinfo.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerinfo.h
+//  teeest
+//
+//  Created by Janus Broch Mols on 09/11/14.
+//  Copyright (c) 2014 Janus Broch Mols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerinfo : UIViewController
+
+@end
---
+++ teeest/ViewControllerinfo.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerinfo.m
+//  teeest
+//
+//  Created by Janus Broch Mols on 09/11/14.
+//  Copyright (c) 2014 Janus Broch Mols. All rights reserved.
+//
+
+#import "ViewControllerinfo.h"
+
+@interface ViewControllerinfo ()
+
+@end
+
+@implementation ViewControllerinfo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
