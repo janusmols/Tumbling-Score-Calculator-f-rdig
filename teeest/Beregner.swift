52217ee9f47e96d96a2419acb697db4cefaf4c89
janusmols <janusbm@gmail.com>
2015-07-06 20:14:21 +0200

    .

--- teeest/Beregner.swift
+++ teeest/Beregner.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 var g_Choose : Int = 1
+var g_SelectedMoment : String = "Tom"
+var ræekke : Int!
+var container: [[[[[[String]]]]]] = [[[[[["Tom                                                                                                                                       0.0","Tom","Tom","Tom","Tom","Tom","Tom","Tom"],["Tom","Tom","Tom","Tom","Tom","Tom","Tom","Tom"],["Tom","Tom","Tom","Tom","Tom","Tom","Tom","Tom"],["Tom","Tom","Tom","Tom","Tom","Tom","Tom","Tom"],["Tom","Tom","Tom","Tom","Tom","Tom","Tom","Tom"],["Tom","Tom","Tom","Tom","Tom","Tom","Tom","Tom"]]]]]]
 
 class Beregner: UIViewController {
     
-    var c1 = 1
-    var c2 = 2
-    var b1 = 3
-    var b2 = 4
-    var e1 = 5
-    var e2 = 6
     
-    var container: [Int: String]!
+    
+    var c1 = 0
+    var c2 = 1
+    var b1 = 2
+    var b2 = 3
+    var e1 = 4
+    var e2 = 5
+    
+    
+    
+
 
     @IBOutlet weak var CRækkeOne: UIButton!
     @IBOutlet weak var CRækkeTwo: UIButton!
@@ -41,6 +48,11 @@ class Beregner: UIViewController {
     @IBOutlet weak var samletResult: UILabel!
     @IBOutlet weak var Result: UILabel!
     
+    @IBOutlet weak var ChooseOne: UIButton!
+    @IBOutlet weak var ChooseTwo: UIButton!
+    @IBOutlet weak var ChooseThree: UIButton!
+    @IBOutlet weak var ChooseFour: UIButton!
+    @IBOutlet weak var ChooseFive: UIButton!
     @IBOutlet weak var MomentSix: UILabel!
     @IBOutlet weak var MomentSeven: UILabel!
     @IBOutlet weak var MomentEight: UILabel!
@@ -66,34 +78,57 @@ class Beregner: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        container[1] = "" as String
-        PasteSelectedMomentInLabels()
-    
-        if (momentTitleOne.text == ""){
-            momentTitleOne.text = "Tom"
+        if ræekke == nil{
+            ræekke = 4
+            EOne()
         }
-        
-        
-
-        EliteKrav.hidden = false
-        BegynderKrav.hidden = true
-        
-        EliteKrav.text = "￼￼\nMini og Junior: Øvelse 1: 8 spring - Fri øvelse- minimum 3\nsaltospring.\nSenior: Øvelse 1: 8 spring - Fri øvelse - minimum 3 saltospring - Max en halv skrue."
-        
-        OtteMoment()
-        
-        CRækkeOne.tintColor = UIColor .grayColor()
-        CRækkeTwo.tintColor = UIColor .grayColor()
-        BRækkeOne.tintColor = UIColor .grayColor()
-        BRækkeTwo.tintColor = UIColor .grayColor()
-        EliteRækkeOne.tintColor = UIColor .blueColor()
-        EliteRækkeTwo.tintColor = UIColor .grayColor()
+        else if ræekke == 0 {
+            COne()
+        }
+        else if ræekke == 1 {
+            CTwo()
+        }
+        else if ræekke == 2 {
+            BOne()
+        }
+        else if ræekke == 3 {
+            BTwo()
+        }
+        else if ræekke == 4 {
+            EOne()
+        }
+        else if ræekke == 5 {
+            ETwo()
+        }
+        PasteSelectedMomentInLabels()
 
     }
 
     @IBAction func CRækkeOne(sender: AnyObject) {
-        FemMoment()
-        
+        COne()
+    }
+    @IBAction func CRækkeTwo(sender: AnyObject) {
+        CTwo()
+    }
+    @IBAction func BRækkeOne(sender: AnyObject) {
+        BOne()
+    }
+    @IBAction func BRækkeTwo(sender: AnyObject) {
+        BTwo()
+    }
+    @IBAction func EliteRækkeOne(sender: AnyObject) {
+        EOne()
+    }
+    @IBAction func EliteRækkeTwo(sender: AnyObject) {
+        ETwo()
+    }
+    
+    
+    func COne(){
+        EliteKrav.hidden = true
+        BegynderKrav.hidden = false
+        ræekke = c1
+        hideOrAppearButtonsAndLabels()
         BegynderKrav.text = "￼\nMikro og mini: Øvelse 1: Rondat - Flik - whip - flik -\n(frit slutspring)\nJunior og senior: Øvelse 1: Rondat - Flik - whip - flik - (frit slutspring) I enten øvelse 1 eller øvelse 2 skal\nslutspringet indeholde mindst en hel skrue)"
         
         CRækkeOne.tintColor = UIColor .blueColor()
@@ -102,10 +137,20 @@ class Beregner: UIViewController {
         BRækkeTwo.tintColor = UIColor .grayColor()
         EliteRækkeOne.tintColor = UIColor .grayColor()
         EliteRækkeTwo.tintColor = UIColor .grayColor()
-    }
-    @IBAction func CRækkeTwo(sender: AnyObject) {
-        FemMoment()
         
+        momentTitleOne.text = "Rondat (                             \n0.2"
+        momentTitleTwo.text = "Flikflak f                           \n0.2"
+        momentTitleThree.text = "Whipback ^                           \n0.3"
+        momentTitleFour.text = "Flikflak f                           \n0.2"
+        momentTitleFive.text = container[0][0][0][0][c1][4]
+    
+    }
+    
+    func CTwo(){
+        EliteKrav.hidden = true
+        BegynderKrav.hidden = false
+        ræekke = c2
+        hideOrAppearButtonsAndLabels()
         BegynderKrav.text = "￼\nMikro og mini: Øvelse 2: Rondat - whip - whip - flik -\n(frit slutspring)\nJunior og senior: Øvelse 2: Rondat - whip - whip - flik -\n(frit slutspring) I enten øvelse 1 eller øvelse 2 skal\nslutspringet indeholde mindst en hel skrue)"
         
         CRækkeOne.tintColor = UIColor .grayColor()
@@ -114,13 +159,20 @@ class Beregner: UIViewController {
         BRækkeTwo.tintColor = UIColor .grayColor()
         EliteRækkeOne.tintColor = UIColor .grayColor()
         EliteRækkeTwo.tintColor = UIColor .grayColor()
+        
+        momentTitleOne.text = "Rondat (                             \n0.2"
+        momentTitleTwo.text = "Whipback ^                           \n0.3"
+        momentTitleThree.text = "Whipback ^                           \n0.3"
+        momentTitleFour.text = "Flikflak f                           \n0.2"
+        momentTitleFive.text = container[0][0][0][0][c2][4]
+        
     }
     
-    
-    
-    @IBAction func BRækkeOne(sender: AnyObject) {
-        FemMoment()
-        
+    func BOne(){
+        EliteKrav.hidden = true
+        BegynderKrav.hidden = false
+        ræekke = b1
+        hideOrAppearButtonsAndLabels()
         BegynderKrav.text = "\nMini, junior og senior: Øvelse 1: 5 spring - Fri øvelse - minimum 2 saltospring"
         
         CRækkeOne.tintColor = UIColor .grayColor()
@@ -129,10 +181,20 @@ class Beregner: UIViewController {
         BRækkeTwo.tintColor = UIColor .grayColor()
         EliteRækkeOne.tintColor = UIColor .grayColor()
         EliteRækkeTwo.tintColor = UIColor .grayColor()
-    }
-    @IBAction func BRækkeTwo(sender: AnyObject) {
-        FemMoment()
         
+        momentTitleOne.text = container[0][0][0][0][b1][0]
+        momentTitleTwo.text = container[0][0][0][0][b1][1]
+        momentTitleThree.text = container[0][0][0][0][b1][2]
+        momentTitleFour.text = container[0][0][0][0][b1][3]
+        momentTitleFive.text = container[0][0][0][0][b1][4]
+        
+    }
+    
+    func BTwo(){
+        EliteKrav.hidden = true
+        BegynderKrav.hidden = false
+        ræekke = b2
+        hideOrAppearButtonsAndLabels()
         BegynderKrav.text = "\nMini, junior og senior: Øvelse 2: 5 spring - Fri øvelse - minimum\n2 saltospring og et spring med hel skrue"
         
         CRækkeOne.tintColor = UIColor .grayColor()
@@ -141,13 +203,19 @@ class Beregner: UIViewController {
         BRækkeTwo.tintColor = UIColor .blueColor()
         EliteRækkeOne.tintColor = UIColor .grayColor()
         EliteRækkeTwo.tintColor = UIColor .grayColor()
-    }
-    
-
-    
-    @IBAction func EliteRækkeOne(sender: AnyObject) {
-        OtteMoment()
         
+        momentTitleOne.text = container[0][0][0][0][b2][0]
+        momentTitleTwo.text = container[0][0][0][0][b2][1]
+        momentTitleThree.text = container[0][0][0][0][b2][2]
+        momentTitleFour.text = container[0][0][0][0][b2][3]
+        momentTitleFive.text = container[0][0][0][0][b2][4]
+        
+    }
+    func EOne(){
+        EliteKrav.hidden = false
+        BegynderKrav.hidden = true
+        ræekke = e1
+        hideOrAppearButtonsAndLabels()
         EliteKrav.text = "￼￼\nMini og Junior: Øvelse 1: 8 spring - Fri øvelse- minimum 3\nsaltospring.\nSenior: Øvelse 1: 8 spring - Fri øvelse - minimum 3 saltospring - Max en halv skrue."
         
         CRækkeOne.tintColor = UIColor .grayColor()
@@ -156,56 +224,93 @@ class Beregner: UIViewController {
         BRækkeTwo.tintColor = UIColor .grayColor()
         EliteRækkeOne.tintColor = UIColor .blueColor()
         EliteRækkeTwo.tintColor = UIColor .grayColor()
+        
+        momentTitleOne.text = container[0][0][0][0][e1][0]
+        momentTitleTwo.text = container[0][0][0][0][e1][1]
+        momentTitleThree.text = container[0][0][0][0][e1][2]
+        momentTitleFour.text = container[0][0][0][0][e1][3]
+        momentTitleFive.text = container[0][0][0][0][e1][4]
+        momentTitleSix.text = container[0][0][0][0][e1][5]
+        momentTitleSeven.text = container[0][0][0][0][e1][6]
+        momentTitleEight.text = container[0][0][0][0][e1][7]
+        
     }
-    @IBAction func EliteRækkeTwo(sender: AnyObject) {
-        OtteMoment()
-        
+    
+    func ETwo(){
+        EliteKrav.hidden = false
+        BegynderKrav.hidden = true
+        ræekke = e2
+        hideOrAppearButtonsAndLabels()
         EliteKrav.text = "￼\nMini og Junior: Øvelse 2: 8 spring - Fri øvelse - minimum 3\nsaltospring og et spring med hel skrue.\nSenior: Øvelse 2: 8 spring - Fri øvelse - minimum 3\nsaltospring og 2 spring med hel skrue"
-        
+    
         CRækkeOne.tintColor = UIColor .grayColor()
         CRækkeTwo.tintColor = UIColor .grayColor()
         BRækkeOne.tintColor = UIColor .grayColor()
         BRækkeTwo.tintColor = UIColor .grayColor()
         EliteRækkeOne.tintColor = UIColor .grayColor()
         EliteRækkeTwo.tintColor = UIColor .blueColor()
-    }
-    
-    
-    func FemMoment(){
-        EliteKrav.hidden = true
-        BegynderKrav.hidden = false
         
-        MomentSix.hidden = true
-        MomentSeven.hidden = true
-        MomentEight.hidden = true
-        ChooseSix.hidden = true
-        ChooseSeven.hidden = true
-        ChooseEight.hidden = true
-        momentTitleSix.hidden = true
-        momentTitleSeven.hidden = true
-        momentTitleEight.hidden = true
-    
-    }
-    
-    func OtteMoment(){
-        EliteKrav.hidden = false
-        BegynderKrav.hidden = true
-        
-        MomentSix.hidden = false
-        MomentSeven.hidden = false
-        MomentEight.hidden = false
-        ChooseSix.hidden = false
-        ChooseSeven.hidden = false
-        ChooseEight.hidden = false
-        momentTitleSix.hidden = false
-        momentTitleSeven.hidden = false
-        momentTitleEight.hidden = false
+        momentTitleOne.text = container[0][0][0][0][e2][0]
+        momentTitleTwo.text = container[0][0][0][0][e2][1]
+        momentTitleThree.text = container[0][0][0][0][e2][2]
+        momentTitleFour.text = container[0][0][0][0][e2][3]
+        momentTitleFive.text = container[0][0][0][0][e2][4]
+        momentTitleSix.text = container[0][0][0][0][e2][5]
+        momentTitleSeven.text = container[0][0][0][0][e2][6]
+        momentTitleEight.text = container[0][0][0][0][e2][7]
         
     }
     
-    
-    
+    func hideOrAppearButtonsAndLabels(){
+        if ((ræekke == 4)||(ræekke == 5)){
+            ChooseSix.hidden = false
+            ChooseSeven.hidden = false
+            ChooseEight.hidden = false
+            MomentSix.hidden = false
+            MomentSeven.hidden = false
+            MomentEight.hidden = false
+            momentTitleSix.hidden = false
+            momentTitleSeven.hidden = false
+            momentTitleEight.hidden = false
+            ChooseOne.hidden = false
+            ChooseTwo.hidden = false
+            ChooseThree.hidden = false
+            ChooseFour.hidden = false
 
+        }
+        else if ((ræekke == 2)||(ræekke == 3)){
+            ChooseSix.hidden = true
+            ChooseSeven.hidden = true
+            ChooseEight.hidden = true
+            MomentSix.hidden = true
+            MomentSeven.hidden = true
+            MomentEight.hidden = true
+            momentTitleSix.hidden = true
+            momentTitleSeven.hidden = true
+            momentTitleEight.hidden = true
+            ChooseOne.hidden = false
+            ChooseTwo.hidden = false
+            ChooseThree.hidden = false
+            ChooseFour.hidden = false
+
+        }
+        else if ((ræekke == 0)||(ræekke == 1)){
+            ChooseSix.hidden = true
+            ChooseSeven.hidden = true
+            ChooseEight.hidden = true
+            MomentSix.hidden = true
+            MomentSeven.hidden = true
+            MomentEight.hidden = true
+            ChooseOne.hidden = true
+            ChooseTwo.hidden = true
+            ChooseThree.hidden = true
+            ChooseFour.hidden = true
+            momentTitleSix.hidden = true
+            momentTitleSeven.hidden = true
+            momentTitleEight.hidden = true
+        }
+    }
+    
     @IBAction func ChooseOne(sender: AnyObject) {
         g_Choose = 1
         performSegueWithIdentifier("PushToChooseTable", sender: self)
@@ -243,36 +348,51 @@ class Beregner: UIViewController {
     func PasteSelectedMomentInLabels(){
         switch (g_Choose) {
   case 1:
-    container[[1][1]] = SelectedMoment as String
-    momentTitleOne.text = SelectedMoment
+    container[0][0][0][0][ræekke][0]
+ = g_SelectedMoment
+    momentTitleOne.text = container[0][0][0][0][ræekke][0]
     break;
     
   case 2:
-    momentTitleTwo.text = SelectedMoment
+    container[0][0][0][0][ræekke][1]
+        = g_SelectedMoment
+    momentTitleTwo.text = container[0][0][0][0][ræekke][1]
     break;
 
   case 3:
-    momentTitleThree.text = SelectedMoment
+    container[0][0][0][0][ræekke][2]
+        = g_SelectedMoment
+    momentTitleThree.text = container[0][0][0][0][ræekke][2]
     break;
             
   case 4:
-    momentTitleFour.text = SelectedMoment
+    container[0][0][0][0][ræekke][3]
+        = g_SelectedMoment
+    momentTitleFour.text = container[0][0][0][0][ræekke][3]
     break;
             
   case 5:
-    momentTitleFive.text = SelectedMoment
+    container[0][0][0][0][ræekke][4]
+        = g_SelectedMoment
+    momentTitleFive.text = container[0][0][0][0][ræekke][4]
     break;
     
   case 6:
-    momentTitleSix.text = SelectedMoment
+    container[0][0][0][0][ræekke][5]
+        = g_SelectedMoment
+    momentTitleSix.text = container[0][0][0][0][ræekke][5]
     break;
             
   case 7:
-    momentTitleSeven.text = SelectedMoment
+    container[0][0][0][0][ræekke][6]
+        = g_SelectedMoment
+    momentTitleSeven.text = container[0][0][0][0][ræekke][6]
     break;
 
   case 8:
-    momentTitleEight.text = SelectedMoment
+    container[0][0][0][0][ræekke][7]
+        = g_SelectedMoment
+    momentTitleEight.text = container[0][0][0][0][ræekke][7]
     break;
             
             
@@ -280,6 +400,7 @@ class Beregner: UIViewController {
   default:
     break;
 }
+        
     }
     
     
---
+++ teeest/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var twoDimensionalString: [[[[[[String]]]]]] = [[[[[["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""]]]]]]
+
+twoDimensionalString[0][0][0][0][0][0]
